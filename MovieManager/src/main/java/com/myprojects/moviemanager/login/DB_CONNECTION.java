8b883d52057af3457c19306e87029f99e8c3e491
Daniel Mic <danielmic000@gmail.com>
2019-12-10 14:41:17 +0200

    Login connection to DB

---
+++ MovieManager/src/main/java/com/myprojects/moviemanager/login/DB_CONNECTION.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myprojects.moviemanager.login;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author danie
+ */
+public class DB_CONNECTION {
+    private static String servername = "localhost";
+    private static String dbname = "java";
+    private static String username = "root";
+    private static String password = "";//no pass
+    private static Integer portnumber = 3306;
+    
+    public static Connection getTheConnection() {
+        
+        Connection connection = null;
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        datasource.setPassword(password);
+        
+        try {
+            connection = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(DB_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return connection;
+    }
+}
--- MovieManager/src/main/java/com/myprojects/moviemanager/login/LOGIN_WINDOW.java
+++ MovieManager/src/main/java/com/myprojects/moviemanager/login/LOGIN_WINDOW.java
@@ -5,6 +5,15 @@
  */
 package com.myprojects.moviemanager.login;
 
+import com.myprojects.moviemanager.main.MAIN_APP_WINDOW;
+import java.awt.Color;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author danie
@@ -30,35 +39,112 @@ public class LOGIN_WINDOW extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        mainPanel = new javax.swing.JPanel();
+        loginTitle = new javax.swing.JLabel();
+        usernameIcon = new javax.swing.JLabel();
+        passwordIcon = new javax.swing.JLabel();
+        usernameField = new javax.swing.JTextField();
+        passwordField = new javax.swing.JPasswordField();
+        loginButton = new javax.swing.JButton();
+        cancelButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setPreferredSize(new java.awt.Dimension(450, 500));
-        jPanel1.setRequestFocusEnabled(false);
+        mainPanel.setBackground(new java.awt.Color(255, 255, 255));
+        mainPanel.setPreferredSize(new java.awt.Dimension(450, 500));
+        mainPanel.setRequestFocusEnabled(false);
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(204, 102, 255));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("LOGIN ");
+        loginTitle.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
+        loginTitle.setForeground(new java.awt.Color(204, 102, 255));
+        loginTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        loginTitle.setText("LOGIN ");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE)
+        usernameIcon.setText("UserIcon");
+
+        passwordIcon.setText("PassIcon");
+
+        usernameField.setForeground(new java.awt.Color(153, 153, 153));
+        usernameField.setText("Username");
+        usernameField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                usernameFieldFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                usernameFieldFocusLost(evt);
+            }
+        });
+        usernameField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernameFieldActionPerformed(evt);
+            }
+        });
+
+        passwordField.setForeground(new java.awt.Color(153, 153, 153));
+        passwordField.setText("password");
+        passwordField.setName("passwordField"); // NOI18N
+        passwordField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                passwordFieldFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                passwordFieldFocusLost(evt);
+            }
+        });
+
+        loginButton.setText("Login");
+        loginButton.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        loginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginButtonActionPerformed(evt);
+            }
+        });
+
+        cancelButton.setText("Cancel");
+
+        javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+        mainPanel.setLayout(mainPanelLayout);
+        mainPanelLayout.setHorizontalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(mainPanelLayout.createSequentialGroup()
+                        .addGap(72, 72, 72)
+                        .addComponent(loginTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, mainPanelLayout.createSequentialGroup()
+                        .addGap(45, 45, 45)
+                        .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(cancelButton, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(passwordIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(usernameIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(usernameField)
+                                    .addComponent(passwordField))))))
                 .addGap(66, 66, 66))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        mainPanelLayout.setVerticalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
                 .addGap(67, 67, 67)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(373, Short.MAX_VALUE))
+                .addComponent(loginTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(52, 52, 52)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(usernameIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(usernameField, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(passwordIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(61, 61, 61)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginButton)
+                    .addComponent(cancelButton))
+                .addContainerGap(116, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -66,17 +152,110 @@ public class LOGIN_WINDOW extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 238, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void usernameFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_usernameFieldActionPerformed
+
+    private void usernameFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usernameFieldFocusGained
+        //removing the placeholder if the textfield == "Username"
+        
+        if(usernameField.getText().trim().toLowerCase().equals("username")) {
+            usernameField.setText("");
+        }
+        usernameField.setForeground(Color.BLACK);
+    }//GEN-LAST:event_usernameFieldFocusGained
+
+    private void usernameFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usernameFieldFocusLost
+        //if nothing is typed in, show the placeholder "Username"
+        //else just change the color to the unfocus preset
+        
+        if(usernameField.getText().trim().toLowerCase().equals("username") ||
+           usernameField.getText().trim().equals("")) 
+        {
+            usernameField.setText("Username");
+        }
+        usernameField.setForeground(new Color(153,153,153));
+    }//GEN-LAST:event_usernameFieldFocusLost
+
+    private void passwordFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordFieldFocusGained
+        //clear the field if the placeholder is == "password"
+        String password = String.valueOf(passwordField.getPassword());
+        if(password.trim().toLowerCase().equals("password")) {
+            passwordField.setText("");
+        }
+        passwordField.setForeground(Color.BLACK);
+    }//GEN-LAST:event_passwordFieldFocusGained
+
+    private void passwordFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordFieldFocusLost
+        // if nothing is typed in, show the placeholder "password"
+        //else just change the color to the unfocus preset
+        String password = String.valueOf(passwordField.getPassword());
+        if(password.trim().toLowerCase().equals("password") || 
+                password.trim().equals(""))
+        {
+            passwordField.setText("password");
+        }
+        passwordField.setForeground(new Color(153,153,153));
+    }//GEN-LAST:event_passwordFieldFocusLost
+
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginButtonActionPerformed
+      
+        String username = usernameField.getText();
+        String password = String.valueOf(passwordField.getPassword());
+        
+    
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        String selectQuery = "SELECT * FROM `users` WHERE `username` = ? AND password = ?";
+        
+        //checking if the fields are empty
+        
+        if(username.trim().toLowerCase().equals("username") ||
+                username.trim().toLowerCase().equals("") ||
+                password.trim().toLowerCase().equals("password")||
+                password.trim().toLowerCase().equals("")) {
+            
+            JOptionPane.showMessageDialog(null,"Please enter your credentials");
+        }
+        else {
+            try {
+                ps = DB_CONNECTION.getTheConnection().prepareStatement(selectQuery);
+                
+                ps.setString(1, username);
+                ps.setString(2,password);
+                
+                rs = ps.executeQuery();
+                
+                if(rs.next()) {
+                       MAIN_APP_WINDOW mainForm = new MAIN_APP_WINDOW();
+                       mainForm.setVisible(true);
+                       mainForm.pack();
+                       
+                       //closing login window
+                       this.dispose();
+                }
+                else {
+                       JOptionPane.showMessageDialog(null,"Showing some error message");
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(LOGIN_WINDOW.class.getName()).log(Level.SEVERE, null, ex);
+            }
+      
+        }
+    }//GEN-LAST:event_loginButtonActionPerformed
+    
     /**
      * @param args the command line arguments
      */
@@ -113,7 +292,13 @@ public class LOGIN_WINDOW extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton cancelButton;
+    private javax.swing.JButton loginButton;
+    private javax.swing.JLabel loginTitle;
+    private javax.swing.JPanel mainPanel;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JLabel passwordIcon;
+    private javax.swing.JTextField usernameField;
+    private javax.swing.JLabel usernameIcon;
     // End of variables declaration//GEN-END:variables
 }
